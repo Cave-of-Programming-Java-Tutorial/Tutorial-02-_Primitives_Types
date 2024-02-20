182a723d146a0a4b0e248e9d660a1fe6fabc35c9
Leonardo Castilho <79671553+CyborgLeunardu@users.noreply.github.com>
2024-02-20 13:32:46 -0300

    Tutorial 02 - Tipos Primitivos
    Short
    Int
    Long
    Float
    Double
    Char
    Boolean
    Byte

---
+++ Tutorial 02 - Primitives Types/src/Primitives.java
@@ -0,0 +1,27 @@
+
+public class Primitives {
+
+	public static void main(String[] args) {
+		short myShort = 01;
+		int myInt = 100;
+		long myLong = 100000000;
+		float myFloat = 10.10f;
+		double myDouble = 100.100;
+		char myChar = 'L';
+		boolean myBoolean = true;
+		byte myByte = 127;
+		
+		System.out.println(myShort);
+		System.out.println(myInt);
+		System.out.println(myLong);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+		
+
+	}
+
+}
